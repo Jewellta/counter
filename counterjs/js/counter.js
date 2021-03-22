dcaf70b5c28a3cb40ce04e5bcb2a5f9fbb0073ce
Tyler Jewell <jewelltyler15@gmail.com>
2021-03-21 23:58:30 -0400

    finished

---
+++ counterjs/js/counter.js
@@ -0,0 +1,23 @@
+
+let nbr =0;
+function loaded(){
+    display();
+}
+
+function decrement(){
+    nbr--;
+    display();
+}
+function increment(){
+    nbr++;
+    display();
+}
+
+function display(){
+    let chg= document.getElementById("display");
+    chg.value= nbr;
+    chg.style.fontStyle= nbr%2==0?"italic": "normal";
+    chg.style.fontWeight= nbr%3==0?"bold": "normal";
+    chg.style.color= nbr%2==0?"red": "black";
+
+}
